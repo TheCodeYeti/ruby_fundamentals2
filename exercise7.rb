3939a56214c6475ed726420ac11a5b12f76fc871
spencer <spencer.dossett@gmail.com>
2016-05-31 16:15:25 -0400

    added display method

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,11 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+def display_students(students)
+  students.each do |cohort, count|
+    puts "#{cohort} : #{count} students"
+  end
+end
+
+display_students(students)
