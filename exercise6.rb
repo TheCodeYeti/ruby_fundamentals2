6cd7b9b7ed83202fcb9eeb2360cb37bdd665986c
spencer <spencer.dossett@gmail.com>
2016-05-31 15:50:42 -0400

    made a method for printing the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,10 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
 
-grocery_list.each do |item|
-  puts "* #{item}"
+def print_grocery_list(grocery_list)
+  puts "#{grocery_list.count} items on the list"
+  grocery_list.each do |item|
+    puts "* #{item}"
+  end
 end
+
+print_grocery_list(grocery_list)
