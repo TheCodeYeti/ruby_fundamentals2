0cb8cdef7d56bcdc763aca3036bc12904ce51bdc
spencer <spencer.dossett@gmail.com>
2016-05-31 16:08:15 -0400

    added a sort option for the method

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,17 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-def print_grocery_list(grocery_list)
+def print_grocery_list(grocery_list, sort_list = false)
+  grocery_list.sort! if sort_list
   puts "#{grocery_list.count} items on the list"
   grocery_list.each do |item|
     puts "* #{item}"
   end
 end
 
+print_grocery_list(grocery_list)
+
+puts "Oops, forgot the rice"
+grocery_list << "rice"
 print_grocery_list(grocery_list)
 
 if grocery_list.include? "bananas"
@@ -16,3 +21,6 @@ else
 end
 
 puts "The second item on the list is #{grocery_list[1]}"
+
+puts "I had better sort this list"
+print_grocery_list(grocery_list, true)
